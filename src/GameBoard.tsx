0e244ebbc0672ef489df02b8546de5ffd1c029d2
magnattic <atticae@gmail.com>
2024-07-31 19:33:54 +0200

    better hexagons

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -11,7 +11,7 @@ const board = [
   { x: 2, y: 5 },
   { x: 3, y: 4 },
   { x: 4, y: 3 },
-  { x: 5, y: 2 },
+  { x: 5, y: 2, word: 'piano' },
 
   { x: 1, y: 8 },
   { x: 2, y: 7 },
@@ -53,46 +53,48 @@ export const GameBoard = () => {
   return (
     <div class="hex-grid">
       <Index each={boardTiles()}>
-        {(item) => <SvgTile x={item().x} y={item().y} />}
+        {(item) => <SvgTile x={item().x} y={item().y} word={item().word} />}
       </Index>
     </div>
   );
 };
 
-const Tile = ({ x, y }: { x: number; y: number }) => {
-  return (
-    <div
-      draggable="true"
-      style={{ 'grid-row': y, 'grid-column': x }}
-      class={` text-slate-700 flex justify-center items-center hexagon bg-slate-50 w-24 h-20 scale-x-150 scale-y-150`}
-    >
-      {x},{y}
-    </div>
-  );
-};
+const SvgTile = ({ x, y, word }: { x: number; y: number; word?: string }) => {
+  const [isHovered, setHovered] = createSignal(false);
+  const hasWord = word !== undefined;
 
-const SvgTile = ({ x, y }: { x: number; y: number }) => {
   return (
     <div
-      class="text-slate-700 flex justify-center items-center scale-130"
+      class={`text-black flex justify-center items-center scale-130 stroke-white`}
+      classList={{ 'stroke-black': hasWord }}
       style={{ 'grid-row': y, 'grid-column': x }}
+      draggable="true"
     >
-      <svg
+      {/* <svg
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
         viewBox="0 0 726 628"
-        class="w-full h-full scale-130"
+        class="w-full h-full scale-130 drop-shadow-md"
       >
         <polygon
           points="723,314 543,625.769145 183,625.769145 3,314 183,2.230855 543,2.230855 723,314"
-          fill="transparent"
-          stroke="blue"
-          stroke-width="10"
+          fill={word ? 'white' : 'transparent'}
+          stroke-width={isHovered() ? 20 : 10}
         />
+      </svg> */}
+      <svg
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        viewBox="0 0 84 84"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M22.0992 77L2.19922 42.5L22.0992 8H61.8992L81.7992 42.5L61.8992 77H22.0992Z"
+          fill={word ? 'white' : 'transparent'}
+          stroke-width={isHovered() ? 3 : 1}
+        ></path>
       </svg>
-      <div class="absolute">
-        {x},{y}
-      </div>
+      <div class="absolute">{word ?? `${x},${y}`}</div>
     </div>
   );
 };
