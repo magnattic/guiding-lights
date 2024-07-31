a8272ca29d3cf1f53721da832e24ab74f7c016ad
magnattic <atticae@gmail.com>
2024-08-01 00:28:40 +0200

    more game logic

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,86 +1,129 @@
-import { createSignal, Index } from 'solid-js';
+import * as R from 'remeda';
+import { createMemo, createSignal, For } from 'solid-js';
+import { createWithSignal } from 'solid-zustand';
+import { words } from './assets/wordList.json';
+import { tileConfig } from './GameBoard/tileConfig';
 
-// Hexagon grid of 8 x 13
-const board = [
-  { x: 1, y: 4 },
-  { x: 2, y: 3 },
-  { x: 3, y: 2 },
-  { x: 4, y: 1 },
+type GameState = {
+  availableTiles: number;
+  placedTiles: TileData[];
+  secrets: TileData[];
+  placeTile: () => void;
+  startGame: () => void;
+};
 
-  { x: 1, y: 6 },
-  { x: 2, y: 5 },
-  { x: 3, y: 4 },
-  { x: 4, y: 3 },
-  { x: 5, y: 2, word: 'piano' },
+type TileData = {
+  x: number;
+  y: number;
+  word?: string;
+  type?: 'treasure' | 'water' | 'trap' | 'curse' | 'amulet' | 'exit';
+};
 
-  { x: 1, y: 8 },
-  { x: 2, y: 7 },
-  { x: 3, y: 6 },
-  { x: 4, y: 5 },
-  { x: 5, y: 4 },
-  { x: 6, y: 3 },
+const isSameTile = (a: TileData, b: TileData) => a.x === b.x && a.y === b.y;
 
-  { x: 1, y: 10 },
-  { x: 2, y: 9 },
-  { x: 3, y: 8 },
-  { x: 4, y: 7 },
-  { x: 5, y: 6 },
-  { x: 6, y: 5 },
-  { x: 7, y: 4 },
+const isAdjacent = (a: TileData, b: TileData) => {
+  if (isSameTile(a, b)) {
+    return false;
+  }
+  const xDiff = Math.abs(a.x - b.x);
+  const yDiff = Math.abs(a.y - b.y);
+  return xDiff <= 1 && yDiff <= 2;
+};
 
-  { x: 2, y: 11 },
-  { x: 3, y: 10 },
-  { x: 4, y: 9 },
-  { x: 5, y: 8 },
-  { x: 6, y: 7 },
-  { x: 7, y: 6 },
+const generateStartingTiles = (): TileData[] => {
+  const startTile = R.sample(tileConfig, 1)[0]!;
+  const adjacentTiles = tileConfig.filter((tile) =>
+    isAdjacent(tile, startTile)
+  );
+  console.log(startTile, adjacentTiles);
+  const randomTiles = [startTile, ...R.sample(adjacentTiles, 2)];
+  const randomWords = R.sample(words, 3);
+  return randomTiles.map((tile, index) => ({
+    ...tile,
+    word: randomWords[index],
+  }));
+};
 
-  { x: 3, y: 12 },
-  { x: 4, y: 11 },
-  { x: 5, y: 10 },
-  { x: 6, y: 9 },
-  { x: 7, y: 8 },
-
-  { x: 4, y: 13 },
-  { x: 5, y: 12 },
-  { x: 6, y: 11 },
-  { x: 7, y: 10 },
-];
-
-export const GameBoard = () => {
-  const [boardTiles, setBoardTiles] = createSignal(board);
-
-  return (
-    <div class="hex-grid">
-      <Index each={boardTiles()}>
-        {(item) => <SvgTile x={item().x} y={item().y} word={item().word} />}
-      </Index>
-    </div>
+const generateSecrets = (startingTiles: TileData[]) => {
+  const freeTiles = tileConfig.filter(
+    (tile) =>
+      !startingTiles.some((startingTile) => isSameTile(tile, startingTile))
+  );
+  const treasures = R.sample(freeTiles, 3);
+  return treasures.map(
+    (treasure) => ({ ...treasure, type: 'treasure' } satisfies TileData)
   );
 };
 
-const SvgTile = ({ x, y, word }: { x: number; y: number; word?: string }) => {
+const useGameState = createWithSignal<GameState>((set) => ({
+  availableTiles: 0,
+  placedTiles: [],
+  secrets: [],
+  placeTile: () =>
+    set((state) => ({ availableTiles: state.availableTiles - 1 })),
+  startGame: () => {
+    const startingTiles = generateStartingTiles();
+    set({
+      availableTiles: 7,
+      placedTiles: startingTiles,
+      secrets: generateSecrets(startingTiles),
+    });
+  },
+}));
+
+export const GameBoard = () => {
+  const availableTiles = useGameState((state) => state.availableTiles);
+  const placedTiles = useGameState((state) => state.placedTiles);
+  const secrets = useGameState((state) => state.secrets);
+  const startGame = useGameState((state) => state.startGame);
+
+  const boardTiles = createMemo((): TileData[] => {
+    return tileConfig.map((tile) => {
+      const placedTile = placedTiles().find((placedTile) =>
+        isSameTile(placedTile, tile)
+      );
+      const secretTile = secrets().find((secret) => isSameTile(secret, tile));
+      return placedTile ?? secretTile ?? tile;
+    });
+  });
+
+  const onStartGame = () => {
+    startGame();
+  };
+
+  return (
+    <main>
+      <button onClick={onStartGame}>Restart Game</button>
+      <div>{availableTiles()} tiles left</div>
+      <div class="hex-grid">
+        <For each={boardTiles()}>
+          {(item) => (
+            <SvgTile x={item.x} y={item.y} word={item.word} type={item.type} />
+          )}
+        </For>
+      </div>
+    </main>
+  );
+};
+
+const SvgTile = ({ x, y, word, type }: TileData) => {
   const [isHovered, setHovered] = createSignal(false);
   const hasWord = word !== undefined;
+  const isTreasure = type === 'treasure';
 
   return (
     <div
-      class={`text-black flex justify-center items-center scale-130 stroke-white`}
+      class={`flex justify-center items-center scale-130`}
+      classList={{
+        'stroke-white': !isTreasure,
+        'stroke-black': hasWord,
+        'stroke-yellow-400': isTreasure,
+        'text-white': !hasWord,
+        'text-black': hasWord,
+      }}
       style={{ 'grid-row': y, 'grid-column': x }}
       draggable="true"
     >
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-        viewBox="0 0 726 628"
-        class="w-full h-full scale-130 drop-shadow-md"
-      >
-        <polygon
-          points="723,314 543,625.769145 183,625.769145 3,314 183,2.230855 543,2.230855 723,314"
-          fill={word ? 'white' : 'transparent'}
-          stroke-width={isHovered() ? 20 : 10}
-        />
-      </svg> */}
       <svg
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
@@ -89,9 +132,8 @@ const SvgTile = ({ x, y, word }: { x: number; y: number; word?: string }) => {
       >
         <path
           d="M22.0992 77L2.19922 42.5L22.0992 8H61.8992L81.7992 42.5L61.8992 77H22.0992Z"
-          fill={word ? 'white' : 'transparent'}
-          stroke-width={isHovered() ? 3 : 1}
-          classList={{ 'stroke-black': hasWord }}
+          fill={word || isTreasure ? 'white' : 'transparent'}
+          stroke-width={isHovered() ? 3 : hasWord ? 2 : 1}
         ></path>
       </svg>
       <div
@@ -99,7 +141,7 @@ const SvgTile = ({ x, y, word }: { x: number; y: number; word?: string }) => {
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
       >
-        {word ?? `${x},${y}`}
+        {word ?? `${x},${y},${type ?? ''}`}
       </div>
     </div>
   );
