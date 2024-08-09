f37076aed751e2c906583f284cbd8fd6e236fe2c
magnattic <atticae@gmail.com>
2024-08-09 20:50:48 +0200

    add explicit height for svg

--- src/Hexagon.tsx
+++ src/Hexagon.tsx
@@ -26,6 +26,8 @@ export const Hexagon = (
     >
       <svg
         {...localProps.svgProps}
+        height="84"
+        width="84"
         viewBox="0 0 84 84"
         xmlns="http://www.w3.org/2000/svg"
       >
